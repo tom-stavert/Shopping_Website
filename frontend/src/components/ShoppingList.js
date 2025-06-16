8a51c6c23bae4376d8017ff37d1dd7da7484909d
Tom Stavert <tom.stavert@gmail.com>
2025-06-16 18:21:08 +0100

    Added back button to shopping list page and fixed class > className

--- frontend/src/components/ShoppingList.js
+++ frontend/src/components/ShoppingList.js
@@ -19,24 +19,29 @@ const ShoppingList = ({recipes, addedRecipes}) => {
   }, [recipes, addedRecipes]);
 
   return(
-    <div className="card border-primary">
-      <div className="card-body">
-        <h5 className="card-title text-center">Shopping List</h5>
-          <table className="table table-sm">
-            <tbody>
-              {shoppingList?.map((ingredient, ingredientId) => (
-                <tr key={ingredientId}>
-                  <td>{ingredient.name}</td>
-                  <td className="text-end">{ingredient.quantity}</td>
-                  <td className="text-start"> {ingredient.unit} </td>
-                  <td>
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-                  </td>
-                </tr>
-              ))
-              }
-            </tbody>
-          </table>
+    <div className="container">
+      <div className="card border-primary">
+        <div className="card-body">
+          <h5 className="card-title text-center">Shopping List</h5>
+            <table className="table table-sm">
+              <tbody>
+                {shoppingList?.map((ingredient, ingredientId) => (
+                  <tr key={ingredientId}>
+                    <td>{ingredient.name}</td>
+                    <td className="text-end">{ingredient.quantity}</td>
+                    <td className="text-start"> {ingredient.unit} </td>
+                    <td>
+                      <input className="form-check-input" type="checkbox" value="" id={ingredientId}></input>
+                    </td>
+                  </tr>
+                ))
+                }
+              </tbody>
+            </table>
+        </div>
+      </div>
+      <div className="row">
+        <Link to="/" className="btn btn-primary">Back</Link>
       </div>
     </div>
   );
