b04e2715d46d0b32e1c2298b34de98a17fd2f589
Tom Stavert <tom.stavert@gmail.com>
2025-07-01 15:27:05 +0100

    Set up SQLite for backend.

--- backend/main.py
+++ backend/main.py
@@ -4,68 +4,69 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Optional, Dict
 from uuid import uuid4
+from sqlmodel import Field, Session , SQLModel, create_engine, select, ForeignKey, Relationship, Table, Column
+
+class IngredientRecipeLink(SQLModel, table=True):
+    ingredient_id: int | None = Field(default=None, foreign_key="ingredientdb.id", primary_key=True)
+    recipe_id: int | None = Field(default=None, foreign_key="recipedb.id", primary_key=True)
+
+    quantity: float | None = Field(default=None)
+    unit: str | None = Field(default=None)
+
+    ingredient: "IngredientDB" = Relationship(back_populates="recipe_links")
+    recipe: "RecipeDB" = Relationship(back_populates="ingredient_links")
+
+class RecipeDB(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field()
+    ingredient_links: list[IngredientRecipeLink] = Relationship(back_populates="recipe")
+
+class IngredientDB(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field()
+    recipe_links: list[IngredientRecipeLink] = Relationship(back_populates="ingredient")
 
 class Ingredient(BaseModel):
-    id: str
+    id: int
     name: str 
     quantity:Optional[float] = None
     unit: Optional[str] = None
 
 class Recipe(BaseModel):
-    id: str
+    id: int
     name: str
     ingredients: List[Ingredient]
 
-# Writing to db when new recipes are made, recipes are deleted, or recipes are updated
-def write_db():
-    with open("recipes.csv", mode='w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["recipe_id", "recipe_name"])
-        for recipe in recipe_db.values():
-            writer.writerow([recipe.id, recipe.name])
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-    with open("ingredients.csv", mode='w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["recipe_id", "ingredient_id", "ingredient_name", "quantity", "unit"])
-        for recipe in recipe_db.values():
-            for ingredient in recipe.ingredients:
-                writer.writerow([
-                    recipe.id,
-                    ingredient.id,
-                    ingredient.name,
-                    ingredient.quantity,
-                    ingredient.unit
-                ])
+engine = create_engine(sqlite_url, echo=True)
 
-# Read csv files to populate recipe_db and ingredients_db when backend server is started
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
 def read_db():
-    with open("recipes.csv", mode='r') as csvfile:
+    with Session(engine) as session:
+        statement = select(RecipeDB, IngredientDB, IngredientRecipeLink).join(RecipeDB).join(IngredientDB)
+        results = session.exec(statement)
         recipe_db = {}
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            recipe_id = row['recipe_id']
-            recipe_db[recipe_id] = Recipe(id=recipe_id, 
-                                          name=row['recipe_name'], 
-                                          ingredients=[])
-    with open("ingredients.csv", mode='r') as csvfile:
         ingredients_db = {}
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            ingredient_id = row["ingredient_id"]
-            ingredient = Ingredient(
-                id=ingredient_id,
-                name=row["ingredient_name"],
-                quantity=float(row["quantity"]),
-                unit=row["unit"]
-            )
-            ingredients_db[ingredient_id] = ingredient
-            recipe_id = row["recipe_id"]
-            if recipe_id in recipe_db:
-                recipe_db[recipe_id].ingredients.append(ingredient)
+        for ing in results:
+            new_ingredient = Ingredient(id=ing.IngredientDB.id,
+                                        name=ing.IngredientDB.name,
+                                        quantity=ing.IngredientRecipeLink.quantity,
+                                        unit=ing.IngredientRecipeLink.unit
+                                        )
+            if ing.RecipeDB.id not in recipe_db:
+                recipe_db[ing.RecipeDB.id] = Recipe(id=ing.RecipeDB.id,                                                
+                                                  name=ing.RecipeDB.name, 
+                                                  ingredients=[new_ingredient])
             else:
-                print(f"Warning: recipe_id {recipe_id} not found in recipe_db.")
-    
-    return recipe_db, ingredients_db
+                recipe_db[ing.RecipeDB.id].ingredients.append(new_ingredient)
+
+            if ing.IngredientDB.id not in ingredients_db:
+                ingredients_db[ing.IngredientDB.id] = Ingredient(id=ing.IngredientDB.id, name=ing.IngredientDB.name)
+    return recipe_db, ingredients_db        
 
 # Updates the ingredients database to add new ingredients from a list, and if the ingredients are already in the database, updates their ID to match
 def update_ingredients_db(ingredients_list):
@@ -114,9 +115,6 @@ app.add_middleware(
 def get_ingredients():
     return recipe_db
 
-# Add a new (empty) recipe to the database
-
-
 # Update an existing recipe given its ID and a recipe object (passed from frontend)
 @app.post("/recipes/{recipe_id}/update")
 def update_recipe(recipe_id: str, recipe: Recipe):
@@ -125,13 +123,11 @@ def update_recipe(recipe_id: str, recipe: Recipe):
         recipe.id = recipe_id
     recipe.ingredients = update_ingredients_db(recipe.ingredients)
     recipe_db[recipe_id] = recipe
-    write_db()
 
 #Delete an existing recipe by its ID
 @app.post("/recipes/{recipe_id}/delete")
 def delete_recipe(recipe_id: str):
     del recipe_db[recipe_id]
-    write_db()
 
 @app.post("/create-shopping-list/", response_model=Dict[str, Ingredient])
 def create_shopping_list(added_recipes: List[str]):
@@ -143,5 +139,6 @@ def ingredients_list():
     return ingredients_db.values()
 
 if __name__ == "__main__":
+    create_db_and_tables()
     recipe_db, ingredients_db = read_db()
     uvicorn.run(app, host="0.0.0.0", port=8000)
